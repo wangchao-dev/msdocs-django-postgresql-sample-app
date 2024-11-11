3a3710e8ba4366da20665a3d79733654ac9fe7f6
wangchao-dev <wangchao@7plus.co.jp>
2024-11-11 10:00:33 +0900

    Update settings.py

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -4,9 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
-
 # Application definition
 INSTALLED_APPS = [
     'restaurant_review.apps.RestaurantReviewConfig',
