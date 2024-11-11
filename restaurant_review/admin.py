85e656f32e8b9757d61115d4fb2c3f91cd42d73e
wangchao-dev <wangchao@7plus.co.jp>
2024-11-11 09:43:25 +0900

    Add files via upload

--- restaurant_review/admin.py
+++ restaurant_review/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import Restaurant, Review
 
 # Register your models here.
--- restaurant_review/views.py
+++ restaurant_review/views.py
@@ -1,10 +1,15 @@
+import uuid
+import os
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render
 from django.db.models import Avg, Count
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.utils import timezone
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.cache import cache_page
+from django.contrib import messages
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
+from requests import RequestException, exceptions
+from azureproject.get_token import get_token
 
 from restaurant_review.models import Restaurant, Review
 
@@ -12,63 +17,118 @@ from restaurant_review.models import Restaurant, Review
 
 def index(request):
     print('Request for index page received')
+    get_token()
     restaurants = Restaurant.objects.annotate(avg_rating=Avg('review__rating')).annotate(review_count=Count('review'))
-    lastViewedRestaurant = request.session.get("lastViewedRestaurant", False)
-    return render(request, 'restaurant_review/index.html', {'LastViewedRestaurant': lastViewedRestaurant, 'restaurants': restaurants})
+    return render(request, 'restaurant_review/index.html', {'restaurants': restaurants })
 
-@cache_page(60)
+
 def details(request, id):
     print('Request for restaurant details page received')
-    restaurant = get_object_or_404(Restaurant, pk=id)
-    request.session["lastViewedRestaurant"] = restaurant.name
-    return render(request, 'restaurant_review/details.html', {'restaurant': restaurant})
+    get_token()
+
+    # Get account_url based on environment
+    account_url = get_account_url()
+    image_path = account_url + "/" + os.environ['STORAGE_CONTAINER_NAME']
+
+    try: 
+        restaurant = Restaurant.objects.annotate(avg_rating=Avg('review__rating')).annotate(review_count=Count('review')).get(pk=id)
+    except Restaurant.DoesNotExist:
+        raise Http404("Restaurant doesn't exist")
+    return render(request, 'restaurant_review/details.html', {'restaurant': restaurant, 
+        'image_path': image_path})
 
 
 def create_restaurant(request):
     print('Request for add restaurant page received')
+
     return render(request, 'restaurant_review/create_restaurant.html')
 
-
-@csrf_exempt
 def add_restaurant(request):
+    get_token()
     try:
         name = request.POST['restaurant_name']
         street_address = request.POST['street_address']
         description = request.POST['description']
-    except (KeyError):
-        # Redisplay the form
-        return render(request, 'restaurant_review/add_restaurant.html', {
-            'error_message': "You must include a restaurant name, address, and description",
-        })
+        if (name == "" or description == ""):
+            raise RequestException()
+    except (KeyError, exceptions.RequestException) as e:
+        # Redisplay the restaurant entry form.
+        messages.add_message(request, messages.INFO, 'Restaurant not added. Include at least a restaurant name and description.')
+        return HttpResponseRedirect(reverse('create_restaurant'))  
     else:
         restaurant = Restaurant()
         restaurant.name = name
         restaurant.street_address = street_address
         restaurant.description = description
         Restaurant.save(restaurant)
-
+                
         return HttpResponseRedirect(reverse('details', args=(restaurant.id,)))
 
-
-@csrf_exempt
 def add_review(request, id):
-    restaurant = get_object_or_404(Restaurant, pk=id)
+    get_token()
+    try: 
+        restaurant = Restaurant.objects.annotate(avg_rating=Avg('review__rating')).annotate(review_count=Count('review')).get(pk=id)
+    except Restaurant.DoesNotExist:
+        raise Http404("Restaurant doesn't exist")
+
     try:
         user_name = request.POST['user_name']
         rating = request.POST['rating']
         review_text = request.POST['review_text']
-    except (KeyError):
-        # Redisplay the form.
-        return render(request, 'restaurant_review/add_review.html', {
-            'error_message': "Error adding review",
-        })
+        if (user_name == "" or rating == ""):
+            raise RequestException()            
+    except (KeyError, exceptions.RequestException) as e:
+        # Redisplay the details page
+        messages.add_message(request, messages.INFO, 'Review not added. Include at least a name and rating for review.')
+        return HttpResponseRedirect(reverse('details', args=(id,)))  
     else:
+
+        if 'reviewImage' in request.FILES:
+            image_data = request.FILES['reviewImage']
+            print("Original image name = " + image_data.name)
+            print("File size = " + str(image_data.size))
+
+            if (image_data.size > 2048000):
+                messages.add_message(request, messages.INFO, 'Image too big, try again.')
+                return HttpResponseRedirect(reverse('details', args=(id,)))  
+
+            # Get account_url based on environment
+            account_url = get_account_url()
+
+            # Create client
+            azure_credential = DefaultAzureCredential()
+            blob_service_client = BlobServiceClient(
+                account_url=account_url,
+                credential=azure_credential)
+
+            # Get file name to use in database
+            image_name = str(uuid.uuid4()) + ".png"
+            
+            # Create blob client
+            blob_client = blob_service_client.get_blob_client(container=os.environ['STORAGE_CONTAINER_NAME'], blob=image_name)
+            print("\nUploading to Azure Storage as blob:\n\t" + image_name)
+
+            # Upload file
+            with image_data as data:
+                blob_client.upload_blob(data)
+        else:
+            # No image for review
+            image_name=None
+
         review = Review()
         review.restaurant = restaurant
         review.review_date = timezone.now()
         review.user_name = user_name
         review.rating = rating
         review.review_text = review_text
+        review.image_name = image_name
         Review.save(review)
-
+                
     return HttpResponseRedirect(reverse('details', args=(id,)))
+
+def get_account_url():
+    # Create LOCAL_USE_AZURE_STORAGE environment variable to use Azure Storage locally. 
+    if 'WEBSITE_HOSTNAME' in os.environ or ("LOCAL_USE_AZURE_STORAGE" in os.environ):
+        return "https://%s.blob.core.windows.net" % os.environ['STORAGE_ACCOUNT_NAME']
+    else:
+        return os.environ['STORAGE_ACCOUNT_NAME']
